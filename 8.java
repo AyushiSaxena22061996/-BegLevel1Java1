3becc6a79c689d9f708ebf6cd0c625f30688ae1f
AyushiSaxena22061996 <ayushisaxena_aditya@srmuniv.edu.in>
2017-07-09 21:03:11 +0530

    Add files via upload

---
+++ 8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,sum;
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter Number");
+		n = s.nextInt();
+		sum = (n*(n+1))/2;
+		System.out.println("Sum ="+sum);
+		
+	}
+}
